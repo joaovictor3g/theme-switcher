5ed69ccad65880e48c862e83e6d1b63928d3ceec
joaovictor3g <jvdias79797@gmail.com>
2020-05-27 15:15:21 -0300

    feat: initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Header: React.FC = () => {
+    return(
+        <Container>
+            Hello
+        </Container>
+    );
+}
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 60px;
+    background: #7159c1;
+    
+`;
